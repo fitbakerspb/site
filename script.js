c768501efbfbc43c877fdce10fb534e6297bde4f
elijahtp <krasilnikoviy@yandex.ru>
2023-08-22 13:42:51 +0000

    1ver

--- script.js
+++ script.js
@@ -270,14 +270,14 @@ if (window.location.pathname.includes('cart.html')) {
 
   btn_order.addEventListener("click", function(){
       const resultPriceTotal_localStorage = JSON.stringify(JSON.parse(localStorage.getItem('resultPriceTotal')));
-      console.log(resultPriceTotal_localStorage);
-      console.log(JSON.stringify(JSON.parse(localStorage.getItem('resultPriceTotal'))));
-      tg.sendData(resultPriceTotal_localStorage);
+      
+      
       if (tg.MainButton.isVisible) {
           tg.MainButton.hide();
       }
       else {
-          tg.MainButton.setText('Оплатить в телеграм или на сайте?');
+          tg.MainButton.setText('Оплатить в телеграм или на сайте??');
+          tg.sendData(resultPriceTotal_localStorage);
           tg.MainButton.show();
       }
   });
